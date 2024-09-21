54d5fd9dd03a8cdc054305091bfa541289babd8a
KNRZZ-LexxDev <166923432+KNRZZ-LexxDev@users.noreply.github.com>
2024-09-21 23:18:49 +0700

    Добавил сайт для Лаб3
    Добавил код для сайта лабораторной работы №3

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Михальченко Алексей">
+        <meta name="description" content ="Веб-сайт для лабораторных работ">
+        <meta name="keywords" contect="Лабораторная, Михальченко">
+        <title>Главная страница</title>
+        <link rel="stylesheet" type="text/css" href="./style/index.css"> <!-- Подключаем CSS файл, если нужно -->
+    </head>
+    <body>
+        <div class="MainPage__Header">
+            <p class="MainPage__Header-text">AlexDevJourney</p>
+        </div>
+
+        <div class="MainPage__Content">
+            <h1 class="MainPage__Content-Head">Сайт для лабораторных работ</h1>
+            <p class="MainPage__Content-Author">Михальченко Алексея</p>
+            <p class="MainPage__Content-Group">Группа ИСП-21</p>
+            <div class="MainPage__Content-LinkWrap">
+                <a href="./page/lab3.php" class="MainPage__Content-Link">Перейти к лабораторной работе №3</a>
+            </div>
+        </div>
+
+        <div class="MainPage__Footer">
+            <p class="MainPage__Footer-text">Все права защищены 2024 &#169</p>
+        </div>
+
+    </body>
+</html>
